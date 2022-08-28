27d735ef874c33019886c32a3cb6752ecb151245
GiORDiA <giordiariel.fullstackdev@gmail.com>
2022-08-28 14:43:59 -0300

    Creación único servicio-obtiene datos completos Porfolio e inyectamos a los componentes

--- src/app/componentes/wave-c/wave-c.component.ts
+++ src/app/componentes/wave-c/wave-c.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-wave-c',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaveCComponent implements OnInit {
 
-  constructor() { }
+  constructor(private datosPorfolio:PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos();
   }
 
 }
---
+++ src/app/servicios/porfolio.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PorfolioService {
+
+  constructor() { }
+
+  obtenerDatos(){
+    console.log("El servicio Porfolio está corriendo");
+  }
+}
